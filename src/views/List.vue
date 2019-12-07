20c6427c29bef5b491fa3430c0a5f37c182a90e9
Chloe Kwon <yoonjeong1104@gmail.com>
2019-12-07 10:50:39 +0900

    add spinner

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="list">
-    <div class="list-item">
+    <button class="btn btn-create" @click="onClickCreate('dev-pvc')">
+      Create
+    </button>
+    <div class="list-item" v-for="item in pvcItems">
       <div class="title">
-        <span>PVC-NAME</span>
+        <span>{{ item.name }}</span>
         <button class="btn btn-edit">Edit</button>
         <button class="btn btn-delete">Delete</button>
       </div>
       <div class="content">
-        <app-spinner v-if="true" />
+        <div class="content__detail" v-if="item.phase !== ''">
+          <pre>
+              name:{{ item.name }}
+              request: {{ item.request }}
+              capacity: {{ item.capacity }}
+              phase: {{ item.phase }}
+          </pre>
+        </div>
+        <app-spinner v-else />
       </div>
     </div>
   </div>
@@ -17,7 +28,23 @@
 import AppSpinner from "@/components/AppSpinner";
 export default {
   name: "List",
-  components: { AppSpinner }
+  components: { AppSpinner },
+  data() {
+    return {
+      pvcItems: []
+    };
+  },
+  methods: {
+    onClickCreate(name) {
+      const newPvc = {
+        name: name,
+        request: "1Gi",
+        capacity: "1Gi",
+        phase: "Bound"
+      };
+      this.pvcItems.push(newPvc);
+    }
+  }
 };
 </script>
 
